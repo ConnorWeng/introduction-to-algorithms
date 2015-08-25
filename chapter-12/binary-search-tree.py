be1e959fbfdfe82ffbaeac41145038b5526f5557
Connor Weng <foamtea30@gmail.com>
2015-08-25 13:19:06 +0800

    improve print_tree

--- chapter-12/binary-search-tree.py
+++ chapter-12/binary-search-tree.py
@@ -24,22 +24,30 @@ def tree_insert(tree, new_node):
             cur_node = cur_node.right
     return tree
 
-def key_with_level(node, level, nodes):
+def key_with_level(node, level, nodes_all_level):
     if node:
-        if len(nodes) > level:
-            nodes[level].append(node.key)
+        if len(nodes_all_level) > level:
+            nodes_all_level[level].append(node)
         else:
-            nodes.append([node.key])
+            nodes_all_level.append([node])
         if node.left:
-            key_with_level(node.left, level+1, nodes)
+            key_with_level(node.left, level+1, nodes_all_level)
         if node.right:
-            key_with_level(node.right, level+1, nodes)
+            key_with_level(node.right, level+1, nodes_all_level)
 
 def print_tree(tree):
-    nodes = []
-    key_with_level(tree, 0, nodes);
-    for node in nodes:
-        print node
+    nodes_all_level = []
+    key_with_level(tree, 0, nodes_all_level);
+    for nodes in nodes_all_level:
+        line = '('
+        last_parent = None
+        for node in nodes:
+            if node.parent != last_parent and line != '(':
+                line += ') ('
+            line += '%d ' % node.key
+            last_parent = node.parent
+        line += ')'
+        print line
 
 def main():
     tree = tree_insert(None, Node(None, 13, None, None))
